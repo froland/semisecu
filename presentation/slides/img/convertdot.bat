4bd9cd0f95541e9df26ea69a504fca909f90123f
mlacroix <lacroix.mika@gmail.com>
2013-03-04 09:30:16 +0100

    script + dotToPdf

--- presentation/slides/img/convertdot.bat
+++ presentation/slides/img/convertdot.bat
@@ -3,7 +3,7 @@ setlocal enableDelayedExpansion
 for %%f in (*.dot) do (
 set filetmp=%%f
 set filename=!filetmp:~0,-4!
-dot.exe -o !filename!".png" -T "png" %%f 
+dot.exe -o !filename!".pdf" -T "pdf" %%f 
 )
 endlocal
 pause
